46692b0214c53a95076a80616b69fd998745dac3
kinghai <kinghai@kinghaimatoiMac.local>
2015-03-09 11:40:18 +0800

    Initial Commit

---
+++ OnecloudVideo/OnecloudVideo.h
@@ -0,0 +1,19 @@
+//
+//  OnecloudVideo.h
+//  OnecloudVideo
+//
+//  Created by kinghai on 15/3/9.
+//  Copyright (c) 2015年 ftguang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OnecloudVideo.
+FOUNDATION_EXPORT double OnecloudVideoVersionNumber;
+
+//! Project version string for OnecloudVideo.
+FOUNDATION_EXPORT const unsigned char OnecloudVideoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OnecloudVideo/PublicHeader.h>
+
+
